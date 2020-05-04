1f3eb1c155e5af6373e1b760b9ef0c23d123b6a1
Luis Filipe Carrete <lfcsanchez@live.com>
2020-05-04 11:39:49 -0300

    .

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MorseActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MorseActivity.java
@@ -27,6 +27,12 @@ public class MorseActivity extends AppCompatActivity {
         Button dash_button = findViewById(R.id.dash_button);
         morseButton(dot_button);
         morseButton(dash_button);
+
+        Button backspace_button = findViewById(R.id.backspace_button);
+        Button space_button = findViewById(R.id.space_button);
+        Button enter_button = findViewById(R.id.enter_button);
+        Button clear_button = findViewById(R.id.clear_button);
+        enterButton(enter_button);
     }
 
     public void nextButton(Button button){
@@ -38,7 +44,7 @@ public class MorseActivity extends AppCompatActivity {
         });
     }
     public void morseButton(Button button){
-        TextView text = findViewById(R.id.screen);
+        TextView text = findViewById(R.id.morse);
         if (button == findViewById(R.id.dot_button)){
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -65,5 +71,15 @@ public class MorseActivity extends AppCompatActivity {
                 }
             });
         }
+    }
+    public void enterButton(Button button){
+        TextView text = findViewById(R.id.morse);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("");
+                frase = null;
+            }
+        });
     }
 }
