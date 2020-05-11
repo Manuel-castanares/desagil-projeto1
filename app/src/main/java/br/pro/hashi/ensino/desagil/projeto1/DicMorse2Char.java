3a43709e1f1d3c9e763ad1f42faccd7799e0282e
Joao Zsigmond <jzsiggy@gmail.com>
2020-05-11 19:06:21 -0300

    Front morse2char

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/DicMorse2Char.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/DicMorse2Char.java
@@ -5,17 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.LinkedList;
 
 public class DicMorse2Char extends AppCompatActivity {
-
+    private Translator translator;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        translator = new Translator();
+
         setContentView(R.layout.activity_dic_morse2_char);
 
         Button back_button = findViewById(R.id.tradutor);
         next_button(back_button);
+        add_list();
     }
 
     private void next_button(Button button) {
@@ -25,5 +33,18 @@ public class DicMorse2Char extends AppCompatActivity {
                 startActivity(new Intent(DicMorse2Char.this, MorseActivity.class));
             }
         });
+    }
+
+    private void add_list() {
+        ListView listView = findViewById(R.id.list);
+        LinkedList<String> codes = translator.getCodes();
+        LinkedList<String> morse = new LinkedList<>();
+
+        for(String code: codes) {
+            morse.add(code + "  =  " + "' " + translator.morseToChar(code) + " '");
+        }
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, morse);
+        listView.setAdapter(arrayAdapter);
     }
 }
